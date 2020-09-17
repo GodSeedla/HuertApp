121728bd014657246ee264c70753245349532a41
Joaquin fuentes <makako24sk8@gmail.com>
2020-09-17 20:16:16 -0300

    Agregado boton de omision al inicio de sesion del usuario : )

--- app/src/main/java/com/example/holamundete/MainActivity.kt
+++ app/src/main/java/com/example/holamundete/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.holamundete
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import com.google.firebase.analytics.FirebaseAnalytics
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private var button: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +21,14 @@ class MainActivity : AppCompatActivity() {
         bundle.putString("message", "Integracion de Firebase completa")
         analytics.logEvent("InitScreen", bundle)
 
+        button = findViewById(R.id.button2)
+        button!!.setOnClickListener(this)
+    }
+
+    override fun onClick(p0: View?) {
+        button2.setOnClickListener{
+            startActivity(Intent(this@MainActivity, MenuP::class.java) )
+        }
     }
 
 
