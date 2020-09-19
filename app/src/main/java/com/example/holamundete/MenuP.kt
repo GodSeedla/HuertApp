30f7a8414981b114d1b50bac6bf09e8afdd48588
RhoVaas <62263901+RhoVaas@users.noreply.github.com>
2020-09-18 21:04:28 -0300

    Se ha logrado establecer coneccion con Firebase y la informacion que contiene este para mostrarlo por un cuadro de texto en la segunda pantalla de la aplicacion

--- app/src/main/java/com/example/holamundete/MenuP.kt
+++ app/src/main/java/com/example/holamundete/MenuP.kt
@@ -2,10 +2,29 @@ package com.example.holamundete
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_menu_p.*
 
-class MenuP : AppCompatActivity() {
+
+class MenuP : AppCompatActivity(), View.OnClickListener {
+    private var info: Button? = null
+    private val db = FirebaseFirestore.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_p)
+
+        info = findViewById(R.id.infoButton)
+        info!!.setOnClickListener(this)
     }
+    override fun onClick(p0: View?) {
+        infoButton.setOnClickListener {
+            db.collection("cultivo").document("ZAna01").get().addOnSuccessListener {
+                cultivoTextView.text = it.get("Descripcion") as String?
+            }
+        }
+    }
+
 }
