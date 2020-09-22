7f4358e555ac7c2f41ac84868c4d4e989fcfdd02
Joaquin fuentes <makako24sk8@gmail.com>
2020-09-22 20:40:03 -0300

    Agregado mensaje de error para el login de la app. Boton registrar no tiene función aún, está por verse.

--- app/src/main/java/com/example/holamundete/huerta_digital.kt
+++ app/src/main/java/com/example/holamundete/huerta_digital.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_huerta_digital.*
 
@@ -21,20 +22,40 @@ class huerta_digital : AppCompatActivity(), View.OnClickListener {
 
     override fun onClick(p0: View?) {
         busquedaButton.setOnClickListener{
-            if (preguntartextView.text.isNotEmpty()){
-                if (preguntartextView.text == "Zanahoria"){
-                    db.collection("cultivo").document("Zanahoria").get().addOnSuccessListener {
-                        nombreCultivoTextView.text = "Zanahoria" as String?
+
+            if (preguntartextView.text.toString().isNotEmpty()){
+              /*   if (preguntartextView.text.toString() != null){
+                    db.collection("cultivo").document(preguntartextView.text.toString()).get().addOnSuccessListener {
+                        nombreCultivoTextView.text = preguntartextView.text.toString()
                         nombreCientificoTextView.text = it.get("Nombre_Cientifico") as String?
                         descripcionTextView.text = it.get("Descripcion") as String?
                     }
                 } else {
                     resultadoBusquedaTextView.text = "No se ha encontrado"
+                }*/
+                busquedaButton.setOnClickListener{
+
+                    var textito = preguntartextView.text.toString()
+
+                    if (textito.isNotEmpty()){
+                        while (db.collection("cultivo").document(textito) != null){
+                            db.collection("cultivo").document(textito).get().addOnSuccessListener {
+                                nombreCultivoTextView.text = textito
+                                nombreCientificoTextView.text = it.get("Nombre_Cientifico") as String?
+                                descripcionTextView.text = it.get("Descripcion") as String?
+                            }
+                            break
+                        }
+                        resultadoBusquedaTextView.text = "No se ha encontrado nada" as String?
+                    }
+
+
                 }
+            }
             }
         }
     }
 
 
-}
+
 
--- app/src/main/java/com/example/holamundete/menu_login.kt
+++ app/src/main/java/com/example/holamundete/menu_login.kt
@@ -6,10 +6,13 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_menu_login.*
 
 class menu_login : AppCompatActivity(), View.OnClickListener {
-    private var button: Button? = null
+    private var omitir: Button? = null
+    private var iniciarSesion: Button? = null
+    private val db = FirebaseFirestore.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,15 +24,58 @@ class menu_login : AppCompatActivity(), View.OnClickListener {
         bundle.putString("message", "Integracion de Firebase completa")
         analytics.logEvent("InitScreen", bundle)
 
-        button = findViewById(R.id.button2)
-        button!!.setOnClickListener(this)
+        omitir = findViewById(R.id.button2)
+        omitir!!.setOnClickListener(this)
+
+        iniciarSesion = findViewById(R.id.button3)
+        iniciarSesion!!.setOnClickListener(this)
     }
 
     override fun onClick(p0: View?) {
         button2.setOnClickListener{
             startActivity(Intent(this@menu_login, menu_principal::class.java) )
+            Clean()
         }
+
+        button3.setOnClickListener{
+            var user = userText.text.toString()
+            var password = PasswordText.text.toString()
+
+            if ((user.isNotEmpty() && password.isNotEmpty())){
+
+                db.collection("usuario").document(user).get().addOnSuccessListener {
+                    if(password == it.get("contraseña")){
+                        startActivity(Intent(this@menu_login, menu_principal::class.java) )
+                        Clean()
+                    }else if((user.isEmpty())&&(password.isEmpty())){
+                        println("PASA PERO NO FUNCA")
+                        errorView.text = "Ingrese una cuenta de usuario" as String? //NO FUNCAAAAA
+                    }else{
+                        errorView.text = "Nombre de usuario o Contraseña incorrecta" as String?
+                    }
+
+                }
+
+            }
+        }
+
     }
 
+    fun Clean(){
+        userText.text = null
+        PasswordText.text = null
+        errorView.text = null
+    }
 
 }
+
+/*if((user == it.get("usuario")) &&
+password == it.get("contraseña")){
+    println("A")
+    startActivity(Intent(this@menu_login, menu_principal::class.java) )
+
+}else {
+    println("stabrea")
+}
+
+ */
