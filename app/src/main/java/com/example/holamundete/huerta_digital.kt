008eccdae32339b22953f5e7c973c2939fc3658f
RhoVaas <62263901+RhoVaas@users.noreply.github.com>
2020-09-21 23:13:04 -0300

    Se modifica la vista "activity_huerta_digital" para ver cómo reacciona esta con la BD

--- app/src/main/java/com/example/holamundete/huerta_digital.kt
+++ app/src/main/java/com/example/holamundete/huerta_digital.kt
@@ -2,10 +2,39 @@ package com.example.holamundete
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_huerta_digital.*
 
-class huerta_digital : AppCompatActivity() {
+class huerta_digital : AppCompatActivity(), View.OnClickListener {
+    private var busq: Button? = null
+    private val db = FirebaseFirestore.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_huerta_digital)
+
+        busq = findViewById(R.id.busquedaButton)
+        busq!!.setOnClickListener(this)
     }
+
+    override fun onClick(p0: View?) {
+        busquedaButton.setOnClickListener{
+            if (preguntartextView.text.isNotEmpty()){
+                if (preguntartextView.text == "Zanahoria"){
+                    db.collection("cultivo").document("Zanahoria").get().addOnSuccessListener {
+                        nombreCultivoTextView.text = "Zanahoria" as String?
+                        nombreCientificoTextView.text = it.get("Nombre_Cientifico") as String?
+                        descripcionTextView.text = it.get("Descripcion") as String?
+                    }
+                } else {
+                    resultadoBusquedaTextView.text = "No se ha encontrado"
+                }
+            }
+        }
+    }
+
+
 }
+
